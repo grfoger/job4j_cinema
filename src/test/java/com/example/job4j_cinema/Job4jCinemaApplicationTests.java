5cdc0bd58b815ecbe0a8efe010bbc6f64edac39d
Arhip <grfoger@gmail.com>
2022-05-23 16:39:02 +0300

    init commit

---
+++ src/test/java/com/example/job4j_cinema/Job4jCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job4j_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
